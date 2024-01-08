675c0810c021c72fcb62903ae37c33b87a224baa
Roberto <roberto.quadraccia90@gmail.com>
2024-01-08 15:19:34 +0100

    TimerChallenge component setup complete

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -9,28 +9,42 @@ import { useState, useRef } from "react";
 // <input ref={playerName} .../> and the input component is connected to the useRef
 
 const Player = () => {
-
   const playerName = useRef();
 
-  const [player, setPlayer] = useState("");
-  const [submitted, setSubmitted] = useState(false);
+  const [player, setPlayer] = useState(null);
+  // const [submitted, setSubmitted] = useState(false);
 
-  const handleChange = (e) => {
-    e.preventDefault();
-    setSubmitted(false);
-    setPlayer(e.target.value);
-  };
+  // const handleChange = (e) => {
+  //   e.preventDefault();
+  //   setSubmitted(false);
+  //   setPlayer(e.target.value);
+  // };
 
   const handleClick = () => {
-    playerName.current
-    // We can now access the input
+    setPlayer(playerName.current.value);
+    // We now get access to the input element. First, we have to access a current property.
+    // This ref value you are getting back from useRef will always be a JavaScript object
+    // that will always have only a current property.
+    // It is in the current property that the actual ref value (connected input in this case) will be stored.
+    // We can now update the state and pass this value which we are getting directly from the input field as value
+    // playerName.current.value
+    // We can get rid of both handleChange function and submitted state, the ref is enough.
+
+    // IMPORTANT: We have to use a state because if we pass playerName.current.value directly below it doesn't work,
+    // because when we render the code for the first time, the connection is not established yet and therefore
+    // playerName.current would be undefined. It is in the next render cycle that we will be able to reach the value.
+    // Another key thing is that whenever ref changes, the component function doesn't re-execute whereas state does.
+    playerName.current.value = '';
   };
 
   return (
     <section id="player">
-      <h2>Welcome {submitted ? player : "unknown entity"}</h2>
+      {/* 
+      ?? is a ternary shortcut, if player is truthy render it, otherwise "unknown entity"
+      */}
+      <h2>Welcome {player ?? 'unknown entity'}</h2>
       <p>
-        <input ref={playerName} type="text" onChange={handleChange} value={player} />
+        <input ref={playerName} type="text" />
         <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
